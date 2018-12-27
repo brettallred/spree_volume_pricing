0f8fe272b3a294a5a84add5b72dc2d43521600b9
Vishal <vishal@bluebash.co>
2018-12-27 17:46:05 +0530

    updated code for reorder

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -10,7 +10,7 @@ Spree::Variant.class_eval do
   def user_prices(user)
     volume_prices.joins('INNER JOIN "spree_pricing_tiers" ON "spree_pricing_tiers"."id" = "spree_volume_prices"."pricing_tier_id"')
                  .joins('LEFT JOIN "spree_pricing_tiers_users" ON "spree_pricing_tiers_users"."pricing_tier_id" = "spree_pricing_tiers"."id" LEFT JOIN "spree_users" ON "spree_users"."id" = "spree_pricing_tiers_users"."user_id" AND "spree_users"."deleted_at" IS NULL ')
-                 .where("spree_pricing_tiers.available_to_all_users = ? OR spree_users.id = ?", true, user.id).distinct.reorder!.order('amount')
+                 .where("spree_pricing_tiers.available_to_all_users = ? OR spree_users.id = ?", true, user.id).distinct.reorder("amount asc")
   end
 
   def join_volume_prices(user = nil)
