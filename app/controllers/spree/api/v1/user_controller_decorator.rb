0aaaa1a18213b80a5e1f07caaef197a6818bd6e8
Vishal <vishal@bluebash.co>
2019-01-22 17:35:11 +0530

    Updated overridden decorator

--- app/controllers/spree/api/v1/user_controller_decorator.rb
+++ app/controllers/spree/api/v1/user_controller_decorator.rb
@@ -1,31 +1,23 @@
-module Spree
-  module Api
-    module V1
-      class UsersController < Spree::Api::BaseController
+Spree::Api::V1::UsersController.class_eval do
+  def index
+    @users = Spree.user_class.accessible_by(current_ability, :read)
 
-      	def index
-          @users = Spree.user_class.accessible_by(current_ability, :read)
-
-          # Customization: Filter users who has already assigned to pricing tier for same supplier
-          if params[:supplier_id].present?
-            supplier = Spree::Supplier.find_by(id: params[:supplier_id])
-            params[:q][:id_not_in] = supplier.pricing_tiers.select('spree_users.id AS user_id').joins(:users).map(&:user_id)
-          end
-
-          @users = if params[:ids]
-                     @users.ransack(id_in: params[:ids].split(','))
-                   else
-                     @users.ransack(params[:q])
-                   end
-
-          @users = @users.result.page(params[:page]).per(params[:per_page])
-
-          expires_in 15.minutes, public: true
-          headers['Surrogate-Control'] = "max-age=#{15.minutes}"
-          respond_with(@users)
-        end
-
-      end
+    # Customization: Filter users who has already assigned to pricing tier for same supplier
+    if params[:supplier_id].present?
+      supplier = Spree::Supplier.find_by(id: params[:supplier_id])
+      params[:q][:id_not_in] = supplier.pricing_tiers.select('spree_users.id AS user_id').joins(:users).map(&:user_id)
     end
+
+    @users = if params[:ids]
+               @users.ransack(id_in: params[:ids].split(','))
+             else
+               @users.ransack(params[:q])
+             end
+
+    @users = @users.result.page(params[:page]).per(params[:per_page])
+
+    expires_in 15.minutes, public: true
+    headers['Surrogate-Control'] = "max-age=#{15.minutes}"
+    respond_with(@users)
   end
 end
